9b9101f3f5f6a2583aff85634e57251010058d6a
Raymond Dowe <raymond.dowe@Raymonds-MacBook-Pro.local>
2016-12-15 12:10:15 +0000

    Adding the ability to flash

--- light.js
+++ light.js
@@ -3,54 +3,97 @@ var HID = require('node-hid');
 
 
 var Light = function () {
+
+    var ctx = this;
     var vendor = 7476;
     var product = 4;
-    this.hid = new HID.HID(vendor, product);
+
+    var hid = new HID.HID(vendor, product);
+    var instructions = [];
+    var timer = null;
+    var running = false;
 
     // define common colours
     this.COLOR_OFF = [0,0,0];
     this.COLOR_RED = [255, 0, 0];
     this.COLOR_GREEN = [0, 255, 0];
     this.COLOR_BLUE = [0, 0, 255];
-    this.COLOR_YELLOW = [255, 255, 0];
     this.COLOR_CYAN = [0, 255, 255];
     this.COLOR_PINK = [255, 0, 255];
     this.COLOR_WHITE = [255, 255, 255];
+    this.COLOR_YELLOW = [255, 255, 0];
+    this.COLOR_ORANGE = [255, 127, 127];
 
     // initialize the light
-    this.hid.write([0x00, 0x1F, 0x02, 0x00, 0x5F, 0x00, 0x00, 0x1F, 0x03]);
-    this.hid.write([0x00, 0x00, 0x02, 0x00, 0x5F, 0x00, 0x00, 0x1F, 0x04]);
-    this.hid.write([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1F, 0x05]);
-    this.hid.write([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01]);
+    hid.write([0x00, 0x1F, 0x02, 0x00, 0x5F, 0x00, 0x00, 0x1F, 0x03]);
+    hid.write([0x00, 0x00, 0x02, 0x00, 0x5F, 0x00, 0x00, 0x1F, 0x04]);
+    hid.write([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1F, 0x05]);
+    hid.write([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01]);
 
     // listen for errors
-    this.hid.on("error", function(err)
+    hid.on("error", function(err)
     {
         console.log("HID ERROR");
         console.log(err);
     });
-};
 
-Light.prototype.color = function (color) {
-    var hex_r = parseInt(color[0].toString(16), 16);
-    var hex_g = parseInt(color[1].toString(16), 16);
-    var hex_b = parseInt(color[2].toString(16), 16);
-    this.write([0x00, hex_r, hex_g, hex_b, 0x00, 0x00, 0x00, 0x1F, 0x05]);
-    return this;
-};
+  this.color = function (color) {
+      var hex_r = parseInt(color[0].toString(16), 16);
+      var hex_g = parseInt(color[1].toString(16), 16);
+      var hex_b = parseInt(color[2].toString(16), 16);
+      ctx.write([0x00, hex_r, hex_g, hex_b, 0x00, 0x00, 0x00, 0x1F, 0x05]);
+      return this;
+  };
 
-Light.prototype.write = function (arBytes) {
-    this.hid.write(arBytes);
-};
+  this.addInstruction = function(color, duration) {
+    instructions.push({
+        color:color,
+        duration:duration
+      });
 
-Light.prototype.close = function() {
-  this.hid.close();
+      if (running == false)
+      {
+        ctx.run();
+      }
+  }
+
+  this.run = function()
+  {
+    if (running == false)
+    {
+      running = true;
+      ctx.nextInstruction();
+    }
+  }
+
+  this.nextInstruction = function()
+  {
+    if (instructions.length > 0)
+    {
+      var first_instruction = instructions[0];
+      instructions.splice(0, 1);
+      timer = setTimeout(ctx.nextInstruction, first_instruction.duration);
+      ctx.color(first_instruction.color);
+    }
+    else
+    {
+      running = false;
+      console.log("Done");
+    }
+  }
+
+  this.write = function (arBytes) {
+      hid.write(arBytes);
+  };
+
+  this.close = function() {
+    hid.close();
+  }
+
+  this.off = function () {
+      ctx.color(this.COLOR_OFF);
+  };
 }
-
-Light.prototype.off = function () {
-    this.color(this.COLOR_OFF);
-    return this;
-};
 
 // make notifier avilable
 var light = new Light();
--- warner.js
+++ warner.js
@@ -1,15 +1,17 @@
 
 var light = require('./light.js');
 
-// change to a custom colour
-var red = 255;
-var green = 255;
-var blue = 255;
-var my_color = [red, green, blue];
-light.color(my_color);
-
-// change to a predefined colour (see light.js for options)
-light.color(light.COLOR_BLUE);
+var flash_speed = 50;
+for (var i = 0; i < 50; i++)
+{
+  light.addInstruction(light.COLOR_RED, flash_speed);
+  light.addInstruction(light.COLOR_BLUE, flash_speed);
+  light.addInstruction(light.COLOR_GREEN, flash_speed);
+  light.addInstruction(light.COLOR_PINK, flash_speed);
+  light.addInstruction(light.COLOR_CYAN, flash_speed);
+  light.addInstruction(light.COLOR_YELLOW, flash_speed);
+  light.addInstruction(light.COLOR_WHITE, flash_speed);
+}
 
 // set the USB to 'OFF' when program ends
 process.on('SIGINT', function () {
